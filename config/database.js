04e3799e86c1516f8c5e3962cf0eda6b34c91415
namae101 <sovannarongkoek@gmail.com>
2020-11-26 15:50:22 +0700

    add postgres config

--- config/database.js
+++ config/database.js
@@ -3,9 +3,14 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      "settings": {
+        "client": "postgres",
+        "host": env(DATABASE_HOST,""),
+        "port": env(DATABASE_PORT,""),
+        "database": env(DATABASE_NAME,""),
+        "username": env(DATABASE_USERNAME,""),
+        "password": env(DATABASE_PASSWORD,""),
+        "ssl": { "rejectUnauthorized": false }
       },
       options: {
         useNullAsDefault: true,
