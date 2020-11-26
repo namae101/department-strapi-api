059d1fe3d93bb92d6d9a2731fda01c4ae15ea64c
namae101 <sovannarongkoek@gmail.com>
2020-11-26 15:56:36 +0700

    fix var to strng

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       "settings": {
         "client": "postgres",
-        "host": env(DATABASE_HOST,""),
-        "port": env(DATABASE_PORT,""),
-        "database": env(DATABASE_NAME,""),
-        "username": env(DATABASE_USERNAME,""),
-        "password": env(DATABASE_PASSWORD,""),
+        "host": env("DATABASE_HOST",""),
+        "port": env("DATABASE_PORT",""),
+        "database": env("DATABASE_NAME",""),
+        "username": env("DATABASE_USERNAME",""),
+        "password": env("DATABASE_PASSWORD",""),
         "ssl": { "rejectUnauthorized": false }
       },
       options: {
